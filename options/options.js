a593769d1ae89059e733a7070467a1dcad47fcdd
Crowdbotics <engineering@crowdbotics.com>
2023-02-09 02:07:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-pine-dev-62248",
+  url: "https://ancient-pine-dev-62248.botics.co",
+  api: "https://ancient-pine-dev-62248.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
